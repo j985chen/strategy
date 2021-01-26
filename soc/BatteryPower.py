9a632631e48b5572db127ee163c4b9bd5d8f16e7
Janet Chen <j985chen@uwaterloo.ca>
2021-01-25 20:05:51 -0500

    removed pandas, initialized resistance directly, renamed calc_power to get_power

--- soc/BatteryPower.py
+++ soc/BatteryPower.py
@@ -1,13 +1,10 @@
-import pandas as pd
 import sys
 import os.path
 
 class BatteryPower:
-    def __init__(self): 
-        self.resistance = 0.055
+    self.resistance = 0.055
 
     #based on BATTERY_AGGREGATE_VC data which has voltage + current out of battery pack
     #unsure how to include the BATTERY_VT, which includes voltage + temperature out of one module?
-    def calc_power(self, current, voltage):
-        power = voltage * current
-        return power
+    def get_power(self, current, voltage):
+        return current * voltage
